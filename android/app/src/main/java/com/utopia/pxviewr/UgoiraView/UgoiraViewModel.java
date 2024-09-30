9893568e40955d64afeb1222189b4d6bd81f63f1
Product Migrator <genbee@example.com>
2024-09-30 06:59:34 +0000

    Migration result task_id 7e3ddabb-82b5-4721-b45a-e7051aacc739

---
+++ android/app/src/main/java/com/utopia/pxviewr/UgoiraView/UgoiraViewModel.java
@@ -0,0 +1,39 @@
+package com.utopia.pxviewr.UgoiraView;
+
+import android.graphics.Bitmap;
+
+public class UgoiraViewModel {
+    private String uri;
+    private Integer delay;
+    private Bitmap bitmap;
+
+    public UgoiraViewModel(String uri, Integer delay, Bitmap bitmap) {
+        this.uri = uri;
+        this.delay = delay;
+        this.bitmap = bitmap;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+
+    public Integer getDelay() {
+        return delay;
+    }
+
+    public void setDelay(Integer delay) {
+        this.delay = delay;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+}
